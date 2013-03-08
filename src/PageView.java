0ce4762802ec60321c057c1f4c3d6018ae76cfc2
mdfeist <mdfeist@ualberta.ca>
2013-03-08 14:41:58 -0700

    Added the ability to add custom options

--- src/PageView.java
+++ src/PageView.java
@@ -17,96 +17,124 @@ abstract public class PageView implements Callback
 	protected ArrayList<DatabaseRow> rows = null;
 	protected String pageTitle = null;
 	
+	protected boolean run;
+	protected int pageNumber;
+	protected int elementIndex;
+	protected int pageSize;
+	
 	abstract public void view();
 	abstract public void getRowInfo(String id);
 	
-	public void pageView(int pageSize)
+	public void printCustomOptions() {}
+	public boolean doCustomOptions() { return false; }
+	
+	public void printOptions()
 	{
+		System.out.println("\nOptions");
+		System.out.println("s: to select");
+		System.out.println("p: previous page");
+		System.out.println("n: next page");
+		printCustomOptions();
+		System.out.println("q: to exit");
+	}
+	
+	protected void selectCommand()
+	{
+		System.out.println("Enter the row number:");
+		String input = Menu.getKeyBoard();
+		
+		Integer adNumber = null;
+		
+		try
+		{
+			adNumber = new Integer(input);
+		} catch (Exception e) {
+			System.out.println("Error: Not an Integer.");
+			return;
+		}
+		
+		int row = adNumber.intValue();
+		
+		if (row >= 1 && row <= rows.size())
+		{
+			getRowInfo(rows.get(row - 1).getID());
+		}
+		
+		elementIndex = pageSize*(pageNumber - 1);
+	}
+	
+	protected void prevCommand()
+	{
+		if (pageNumber > 1)
+		{
+			pageNumber--;
+			elementIndex = pageSize*(pageNumber - 1);
+		} else
+		{
+			elementIndex = pageSize*(pageNumber - 1);
+		}
+	}
+	
+	protected void nextCommand()
+	{
+		if (elementIndex < rows.size())
+		{
+			elementIndex = pageSize*pageNumber;
+			pageNumber++;
+		} else
+		{
+			elementIndex = pageSize*(pageNumber - 1);
+		}
+	}
+	
+	protected void stop()
+	{
+		run = false;
+	}
+	
+	public void doCommand(String command)
+	{
+		if (command.equals("s")) { selectCommand(); } 
+		else if (command.equals("p")) { prevCommand(); }
+		else if (command.equals("n")) { nextCommand(); }
+		else if (command.equals("q")) { stop(); }
+		else if (doCustomOptions()) {}
+		else
+		{
+			elementIndex = pageSize*(pageNumber - 1);
+			System.out.println("Error: Unknown command.");
+		}
+	}
+	
+	public void pageView(int size)
+	{
+		pageSize = size;
+		
 		if (rows == null)
 		{
 			System.out.println("\nERROR: No rows in view.");
 			return;
 		}
 		
-		boolean run = true;
-		int pageNumber = 1;
-		int i = 0;
+		run = true;
+		pageNumber = 1;
+		elementIndex = 0;
 		while (run)
 		{
 			if (pageTitle != null)
 				System.out.println(pageTitle);
 			
-			while (i < pageSize*pageNumber && i < rows.size())
+			while (elementIndex < pageSize*pageNumber && elementIndex < rows.size())
 			{
-				System.out.println((i + 1) + ": " + rows.get(i).toString());
-				i++;
+				System.out.println((elementIndex + 1) + ": " + rows.get(elementIndex).toString());
+				elementIndex++;
 			}
 			
-			System.out.println("\nOptions");
-			System.out.println("s: to select");
-			System.out.println("p: previous page");
-			System.out.println("n: next page");
-			System.out.println("q: to exit");
+			printOptions();
 			
 			String command = Menu.getKeyBoard();
 			
-			switch (command.toCharArray()[0])
-			{
-				case 's':
-					System.out.println("Enter the row number:");
-					String input = Menu.getKeyBoard();
-					
-					Integer adNumber = null;
-					
-					try
-					{
-						adNumber = new Integer(input);
-					} catch (Exception e) {
-						System.out.println("Error: Not an Integer.");
-						break;
-					}
-					
-					int row = adNumber.intValue();
-					
-					if (row >= 1 && row <= rows.size())
-					{
-						getRowInfo(rows.get(row - 1).getID());
-					}
-					
-					i = pageSize*(pageNumber - 1);
-					break;
-					
-				case 'p':
-					if (pageNumber > 1)
-					{
-						pageNumber--;
-						i = pageSize*(pageNumber - 1);
-					} else
-					{
-						i = pageSize*(pageNumber - 1);
-					}
-					break;
-					
-				case 'n':
-					if (i < rows.size())
-					{
-						i = pageSize*pageNumber;
-						pageNumber++;
-					} else
-					{
-						i = pageSize*(pageNumber - 1);
-					}
-					break;
-					
-				case 'q':
-					run = false;
-					break;
-					
-				default:
-					i = pageSize*(pageNumber - 1);
-					System.out.println("Error: Unknown command.");
-					break;
-			}
+			doCommand(command);
 		}
 	}
 	
